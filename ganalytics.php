2ad55e8d4609de6d91f29b388ebfe238d3fa0db8
Thomas N <Quetzacoalt91@users.noreply.github.com>
2015-01-29 14:06:37 +0100

    Update ganalytics.php

--- ganalytics.php
+++ ganalytics.php
@@ -35,7 +35,7 @@ class Ganalytics extends Module
 {
 	protected $js_state = 0;
 	protected $eligible = 0;
-	protected $filterable = 1;
+	protected $filterable = 1
 
 	public function __construct()
 	{
